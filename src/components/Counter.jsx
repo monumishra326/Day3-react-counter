d5c5bc5b696d16959d3f68860f765bf7dc81130c
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-04 17:14:03 +0530

    day3

---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import {useState, use} from "react";
+import "../components/counter.css"
+
+export function Counter(value){
+    const [counter,setCounter] =useState(10);
+    const handlechange=(value)=>{
+        setCounter(counter+value);
+    };
+    const handlechange1=(value)=>{
+        setCounter((counter)*2);
+    };
+    
+    
+
+
+
+   
+    return ( 
+        <div>
+    <h3>Counter:{counter}</h3>
+    <button onClick={()=> handlechange(1)}> Add </button>
+    <button onClick={()=>{
+        if(counter>=1){
+            handlechange(-1)
+        }
+    } }> Decrease</button>
+    
+    <button onClick={()=>handlechange1(1)}>Double Button</button>
+    </div>
+    );
+};
